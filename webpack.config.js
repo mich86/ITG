ffa02c9315e6a6d380caeaab5e9a0d7088f2d405
Michelle Campbell <m.campbell.tkd@gmail.com>
2026-06-29 17:53:33 +0100

    webpack bundle splitting and contenthash caching to improve Lighthouse performance, robots.txt to improve SEO score

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,34 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+module.exports = (env, argv) => {
+    const isProd = argv.mode === "production";
+
+    return {
     entry: {
         index: path.resolve(__dirname, "src", "index.js")
     },
     output: {
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        filename: isProd ? "[name].[contenthash].js" : "[name].js"
     },
+    devtool: isProd ? "source-map" : "eval-cheap-module-source-map",
     devServer: {
         hot: true,
         inline: true,
         open: true,
         contentBase: path.join(__dirname, 'public')
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendor",
+                    chunks: "all"
+                }
+            }
+        }
     },
     module: {
         rules: [
@@ -45,8 +61,13 @@ module.exports = {
                 {
                     from: path.resolve(__dirname, "public", "images"),
                     to: "images"
+                },
+                {
+                    from: path.resolve(__dirname, "public", "robots.txt"),
+                    to: "robots.txt"
                 }
             ],
         }),
     ]
+    };
 };
